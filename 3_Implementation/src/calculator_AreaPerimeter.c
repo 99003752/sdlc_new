69dc589edd48475ab064a36ec5847815ef09855c
99003729 <pushkar.antony@ltts.com>
2021-03-11 13:50:34 +0530

    anushkas

--- 3_Implementation/src/calculator_AreaPerimeter.c
+++ 3_Implementation/src/calculator_AreaPerimeter.c
@@ -1,12 +1,11 @@
 #include <calculator_AreaPerimeter.h>
 
 
-
 double Area_Circle(double radius)
 {
     double area_of_circle=0;
     {
-        area_of_circle=PI*pow(radius,2);
+        area_of_circle=PI*(radius*radius);
         printf("\narea of circle calculated");
         return area_of_circle;
     }
