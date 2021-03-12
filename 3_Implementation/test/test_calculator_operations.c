b396488f0f1be288999fe59a5480d7fbfa1acda1
99003729 <pushkar.antony@ltts.com>
2021-03-12 12:50:43 +0530

    testcases

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -50,70 +50,70 @@ void test_add(void) {
 void test_add_testcase2(void) {
  
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1500, add(750, 7500));
+  //TEST_ASSERT_EQUAL(1500, add(750, 7500));
   
-  TEST_ASSERT_EQUAL(100, add(250, 750));
+  //TEST_ASSERT_EQUAL(100, add(250, 750));
 }
 void test_subtract(void) {
   TEST_ASSERT_EQUAL(-3, subtract(0, 3));
   TEST_ASSERT_EQUAL(3, subtract(6, 3));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1, subtract(1000, 900));
-  TEST_ASSERT_EQUAL(10, subtract(1000, 90));
+  //TEST_ASSERT_EQUAL(1, subtract(1000, 900));
+  //TEST_ASSERT_EQUAL(10, subtract(1000, 90));
 }
 
 void test_multiply(void) {
   TEST_ASSERT_EQUAL(0, multiply(1, 0));
   TEST_ASSERT_EQUAL(10, multiply(1, 10));
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(2, multiply(2, 5));
+  //TEST_ASSERT_EQUAL(2, multiply(2, 5));
 }
 
 void test_divide(void) {
   TEST_ASSERT_EQUAL(0, divide(1, 0));
   TEST_ASSERT_EQUAL(3, divide(6, 2));
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(3, divide(2, 2));
+  //TEST_ASSERT_EQUAL(3, divide(2, 2));
 }
 void test_correct_pin(void) {
   TEST_ASSERT_EQUAL(0, correct_pin(1234));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1234, correct_pin(7878));
+  //TEST_ASSERT_EQUAL(1234, correct_pin(7878));
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1, correct_pin(798678));
+  //TEST_ASSERT_EQUAL(1, correct_pin(798678));
 }
 void test_convertions(void) 
 {
   TEST_ASSERT_EQUAL(472.441, conversion(1,12));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(111, conversion(1,2222));
+  //TEST_ASSERT_EQUAL(111, conversion(1,2222));
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(4568, conversion(2,33144));
+  //TEST_ASSERT_EQUAL(4568, conversion(2,33144));
 }
 
 void test_AreaPerimeter(void)
 {
    //for choose operation
-  TEST_ASSERT_EQUAL(1, Area_Circle(1)) ;
-  TEST_ASSERT_EQUAL(2, Perimeter_Square(2));
+  TEST_ASSERT_EQUAL(3.14, Area_Circle(1)) ;
+  TEST_ASSERT_EQUAL(8, Perimeter_Square(2));
     /*Dummy Test fail*/
-  TEST_ASSERT_EQUAL("r", Area_Circle(1));
-  TEST_ASSERT_EQUAL("u", Perimeter_Rectangle(2,468));
+  //TEST_ASSERT_EQUAL("r", Area_Circle(1));
+  //TEST_ASSERT_EQUAL("u", Perimeter_Rectangle(2,468));
     
    //for choose shape under area function
-  TEST_ASSERT_EQUAL(1, Area_Circle(1));
-  TEST_ASSERT_EQUAL(2, Perimeter_Square(2));
+  //TEST_ASSERT_EQUAL(1, Area_Circle(1));
+  //TEST_ASSERT_EQUAL(2, Perimeter_Square(2));
    
      /*Dummy Test fail*/
-  TEST_ASSERT_EQUAL("r", Area_Circle(1));
-  TEST_ASSERT_EQUAL("u", Perimeter_Square(2));
+  //TEST_ASSERT_EQUAL("r", Area_Circle(1));
+  //TEST_ASSERT_EQUAL("u", Perimeter_Square(2));
     
    //for choose shape under perimeter function
-  TEST_ASSERT_EQUAL(1, Area_Square(1));
-  TEST_ASSERT_EQUAL(2, Perimeter_Rectangle(2,879));
+  //TEST_ASSERT_EQUAL(1, Area_Square(1));
+  //TEST_ASSERT_EQUAL(2, Perimeter_Rectangle(2,879));
 
 }
 
